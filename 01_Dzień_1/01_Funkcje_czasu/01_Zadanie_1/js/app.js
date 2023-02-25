cf288fcacdde5a945b062790800cebcd259045e7
Embewu <embewu@gmail.com>
2023-02-25 05:05:16 +0100

    ex done

--- 01_Dzień_1/01_Funkcje_czasu/01_Zadanie_1/js/app.js
+++ 01_Dzień_1/01_Funkcje_czasu/01_Zadanie_1/js/app.js
@@ -1,1 +1,13 @@
+// const timeout = setTimeout(function () {
+//     console.log('zadzialam za 5 sekung');
+// }, 5000);
+
+const interval = setInterval(function () {
+    console.log('dzialam co 1 sekunde');
+}, 1000);
+
+
+
+
+
 
